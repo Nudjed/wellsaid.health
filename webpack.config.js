056af7ffddb8e45b6747f5809d3044901d919a94
Lee Driscoll <leedriscoll@me.com>
2018-10-17 11:23:45 +0100

    remove dist folder and allow webpack to do build

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,45 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  entry: './index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'styles.css',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            { loader: 'css-loader', options: { importLoaders: 1 } },
-            'postcss-loader'
-          ]
-        })
-      }
-    ]
-  },
-  plugins: [
-    new ExtractTextPlugin('styles.css')
-  ]
+    entry: './src/styles.css',
+    mode: process.env.NODE_ENV,
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        { loader: 'css-loader', options: { importLoaders: 1 } },
+                        'postcss-loader',
+                    ],
+                }),
+            },
+        ],
+    },
+    plugins: [
+        new ExtractTextPlugin('styles.css', {
+            disable: process.env.NODE_ENV === 'development',
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html',
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'privacy.html',
+            template: 'src/privacy.html',
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'terms.html',
+            template: 'src/terms.html',
+        }),
+        new CopyWebpackPlugin([{
+            from: 'src/assets',
+            to: 'assets',
+            toType: 'dir'
+        }]),
+    ],
 }
